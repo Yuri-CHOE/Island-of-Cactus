3dfdf95a48acebe078bbd344d4b11f3faba27c0f
Yuri-CHOE <78342803+Yuri-CHOE@users.noreply.github.com>
2021-04-29 08:27:52 +0900

    csv file Reader
    csv file reader 추가
    * Resources 바로 아래에 파일을 놔두어야 오류가 안뜹니다....
    CSVReader :  csv 파일을 읽는 스크립트
    CSV_TEST : 읽을 csv 파일 지정 및 전체 출력하는 테스트 코드
    TEST Scene : 실행시 CSV_TEST 코드가 실행되도록 해놨습니다. csv 파일 내용이 console에 출력되니 참고 부탁드립니다

---
+++ Assets/Resources/Script/CustomUI/CSV_TEST.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CSV_TEST : MonoBehaviour
+{
+    public int _exp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<Dictionary<string, object>> data = CSVReader.Read("Text_Table");
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            Debug.Log(data[i]["id"] + " " + data[i]["String"]);
+        }
+    }
+}
