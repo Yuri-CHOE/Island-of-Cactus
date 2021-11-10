92ec45060c827dc7c9412924580a65b218f9c123
juny751 <juny751@gmail.com>
2021-11-10 16:49:45 +0900

    종료 절차 구현
    종료 명령 구현
    -ESC, 안드로이드 뒤로가기
    
    종료 절차 구현
    -세이브 후 종료

---
+++ Assets/Resources/Script/QuitManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class QuitManager : MonoBehaviour
+{
+    public static Coroutine quitSequence = null;
+
+    [SerializeField] Transform box = null;
+
+    [SerializeField] InputAction escape  = null;
+    //[SerializeField] InputActionMap escapeMap  = null;
+
+
+
+    void Start()
+    {
+        //escapeMap.Enable();
+        escape.Enable();
+    }
+
+
+    void Update()
+    {
+        //if (escape.triggered)
+        if (escape.triggered)
+            Call();
+    }
+
+    public void Call()
+    {
+        // 박스 호출
+        box.gameObject.SetActive(true);
+        Debug.Log("디버그 :: esc 감지");
+    }
+
+
+
+    public void Quit()
+    {
+        if(quitSequence != null)
+        {
+            Debug.LogWarning("종료 :: 프로그램 종료 취소 -> 이미 수행중");
+            return;
+        }
+
+        Debug.Log("종료 :: 프로그램 종료 요청됨");
+
+        // 종료 절차 수행
+        quitSequence = StartCoroutine(QuitSequence());
+    }
+
+    IEnumerator QuitSequence()
+    {
+        Debug.Log("종료 :: 프로그램 종료 절차 시작됨");
+        yield return null;
+
+        // 저장 대기
+        while (GameSaveStream.saveControl != null)
+        {
+            Debug.Log("종료 :: 게임 세이브 대기중");
+            yield return null;
+        }
+
+        Debug.Log("종료 :: 프로그램 종료 절차 완료됨");
+
+        // 종료
+        Application.Quit();
+    }
+}
